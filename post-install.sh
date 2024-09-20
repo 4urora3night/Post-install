081e2c619edad4015e44e5e338f767ba6509a11f
4urora3night <auroraph4ntom@gmail.com>
2024-09-20 18:52:31 +0100

    Chromebook update

--- post-install.sh
+++ post-install.sh
@@ -4,40 +4,75 @@ BLUE='\e[34m'
 GREEN='\e[32m'
 RED='\e[31m'
 ENDCOLOR='\e[0m'
-FAINT='\033[3m'
 AURHELPER="yay"
 
-echo -e $GREEN"
-╔═╗┌─┐┌─┐┌┬┐  ╦┌┐┌┌─┐┌┬┐┌─┐┬  ┬  
-╠═╝│ │└─┐ │   ║│││└─┐ │ ├─┤│  │  
-╩  └─┘└─┘ ┴   ╩┘└┘└─┘ ┴ ┴ ┴┴─┘┴─┘"$ENDCOLOR
+echo -e $GREEN"██████╗  ██████╗ ███████╗████████╗    ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     
+██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     
+██████╔╝██║   ██║███████╗   ██║       ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     
+██╔═══╝ ██║   ██║╚════██║   ██║       ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     
+██║     ╚██████╔╝███████║   ██║       ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
+╚═╝      ╚═════╝ ╚══════╝   ╚═╝       ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝"$ENDCOLOR
 
 echo
+
 echo "Starting post-install script... AUR helper will be installed first."
-read -p "$(echo -e "Install "$RED"Yay "$ENDCOLOR"using git?"$FAINT"(y/n)"$ENDCOLOR)" choice
+
+
+read -p "$(echo -e "Install "$RED"Yay "$ENDCOLOR"using git?(y/n)"$ENDCOLOR)" choice
+if [ ! -d ~/repo ] ; then
 mkdir ~/repo
-cd ~/repo
+else 
+
+echo -e $BLUE"~/repo"$ENDCOLOR" already exists."
+
+fi
+
 if [ "$choice" = "y" ]; then
 echo -e "Installing "$RED"git "$ENDCOLOR"and "$RED"base-devel "$ENDCOLOR
     sudo pacman -S --needed git base-devel
+    cd ~/repo
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
     cd ~/repo
 fi
-echo
-echo -e "To continue the post-install, "$RED"Dialog"$ENDCOLOR" is needed to provide a TUI."
-sleep 2
-read -p "$(echo -e "Install "$RED"ML4W"$ENDCOLOR" using AUR(1) or GIT(2)?"$FAINT"(1/2/n)"$ENDCOLOR)" choice
+
+read -p "$(echo -e "Do quick install??"$RED"THIS INCLUDES CHROMEBOOK RELATED STUFF"$ENDCOLOR"(y/n)")" choice
+
+if [ "$choice" = "y" ]; then 
+    cd ~/repo
+    git clone https://github.com/WeirdTreeThing/chromebook-linux-audio.git
+    cd chromebook-linux-audio
+    ./setup-audio
+    cd ~/repo
+    git clone https://github.com/WeirdTreeThing/cros-keyboard-map
+    cd cros-keyboard-map
+    ./install.sh
+    cd ~/repo
+    yay -Syyu
+    yay -S gum rsync
+    git clone https://github.com/mylinuxforwork/dotfiles.git
+    cd dotfiles/bin
+    ./ml4w-hyprland-setup
+elif [ "$choice" = "n" ]; then
+
+read -p "$(echo -e "Install "$BLUE"ML4W"$ENDCOLOR" using AUR(1) or GIT(2)?"$FAINT"(1/2/n)"$ENDCOLOR)" choice
+
 if [ "$choice" = "1" ]; then
-echo -e "Installing "$RED"ML4W"$ENDCOLOR"using"$GREEN"AUR"$ENDCOLOR
-    yay -Syu 
+echo -e "Installing "$BLUE"ML4W"$ENDCOLOR"using"$GREEN"AUR"$ENDCOLOR
+    yay -Syyu 
     yay -S ml4w-hyprland-git
+    ml4w-hyprland-setup
+
 fi
 if [ "$choice" = "2" ]; then
-echo -e "Installing "$RED"ML4W "$ENDCOLOR"using "$GREEN"Git"$ENDCOLOR"."
-    yay -SYu
+echo -e "Installing "$BLUE"ML4W "$ENDCOLOR"using "$GREEN"Git"$ENDCOLOR"."
+    yay -Syyu
+    yay -S gum rsync
     git clone https://github.com/mylinuxforwork/dotfiles.git
     cd dotfiles/bin
     ./ml4w-hyprland-setup
 fi
+
+fi
+
